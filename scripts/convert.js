b27433af450cb4e4412e23831f60b841bedde20c
Jim Englert <englert.james@gmail.com>
2014-06-15 18:14:00 -0400

    Updates.

--- scripts/convert.js
+++ scripts/convert.js
@@ -1,3 +1,19 @@
 var converter = require('../lib/converter');
 
-converter.readFile("test-api.json");
+if (process.argv.length < 3) {
+	console.log("Error parsing input.");
+	console.log("");
+	console.log("Usage:");
+	console.log("node scripts/convert.js [input filename] [target filename]");
+
+	process.exit(1);
+}
+
+var file = process.argv[2];
+var target = "api.md"; 
+
+if (process.argv.length >= 4) { 
+  target = process.argv[3];
+}
+
+converter.readFile(file, target);
